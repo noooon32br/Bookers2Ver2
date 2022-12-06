6b96425899a412440ca2d3a250d11ffbebabb6ea
EC2 Default User <ec2-user@ip-172-31-26-236.ap-northeast-1.compute.internal>
2022-12-06 08:04:45 +0000

    検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'top' => 'homes#top'
   get '/home/about' => 'homes#about', as: 'about'
+  get "search" => "searches#search"
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
@@ -13,7 +14,6 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
