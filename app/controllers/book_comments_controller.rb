6e312ef43d187f677349e4c5e072fbbb9222192f
EC2 Default User <ec2-user@ip-172-31-26-236.ap-northeast-1.compute.internal>
2022-12-08 13:52:11 +0000

    comment修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,23 +1,15 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    @comment = BookComment.create(book_comment_params)
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_back(fallback_location: root_path) } # 前のページに遷移
-        format.js  # create.js.erbが呼び出される
-      else
-        format.html { redirect_back(fallback_location: root_path) } # 前のページに遷移
-      end
-    end
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    render :create
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
-
   end
 
   private
